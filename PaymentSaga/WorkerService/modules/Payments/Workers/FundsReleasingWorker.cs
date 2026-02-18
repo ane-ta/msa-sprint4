30a39d6b2a038f9aca06aa736358898df253303b
Anna Voyteshchuk <anna.voyteshchuk@gmail.com>
2026-02-18 13:34:42 +0500

    feat: FundsReleasing Worker added

---
+++ PaymentSaga/WorkerService/modules/Payments/Workers/FundsReleasingWorker.cs
@@ -0,0 +1,26 @@
+using WorkerService.modules.Payments.DTOs;
+using Zeebe.Client.Accelerator.Abstractions;
+using Zeebe.Client.Accelerator.Attributes;
+
+namespace WorkerService.modules.Payments.Workers
+{
+	[JobType("FundsReleasing")]
+	public class FundsReleasingWorker : IAsyncZeebeWorkerWithResult<FundsReleasingResultInfo>
+	{
+		private readonly ILogger<FundsReleasingWorker> _logger;
+
+		public FundsReleasingWorker(ILogger<FundsReleasingWorker> logger)
+		{
+			_logger = logger;
+		}
+
+		public async Task<FundsReleasingResultInfo> HandleJob(ZeebeJob job, CancellationToken cancellationToken)
+		{
+			_logger.LogInformation("[{JobKey}] Authorizing payment...", job.Key);
+
+			var info = job.getVariables<FundsReleasingRequestInfo>();
+
+			return new FundsReleasingResultInfo(FundsReleasingStatus.Success, $"Reservation for Order {info.OrderId} released");
+		}
+	}
+}
